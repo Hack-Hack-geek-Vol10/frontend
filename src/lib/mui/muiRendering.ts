1dd8d7c3bf563d63e62a8d1a57e9561572501c7a
taku10101 <taku1011ht@gmail.com>
2023-11-21 10:40:10 +0900

    lib fix

---
+++ src/lib/mui/muiRendering.ts
@@ -0,0 +1,18 @@
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+
+import GitHubIcon from "@mui/icons-material/GitHub";
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
+import PersonAddOutlinedIcon from "@mui/icons-material/PersonAddOutlined";
+import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
+
+export { default as Textarea } from "@mui/joy/Textarea";
+export { default as JoyBadge } from "@mui/joy/Badge";
+export {
+  GitHubIcon,
+  NotificationsNoneOutlinedIcon,
+  MailOutlineIcon,
+  EditOutlinedIcon,
+  PersonAddOutlinedIcon,
+  DeleteOutlineOutlinedIcon,
+};
