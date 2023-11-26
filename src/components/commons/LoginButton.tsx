d6049331ddd43c73e3f029154b3f73b0277eac37
taku10101 <taku1011ht@gmail.com>
2023-11-26 21:07:54 +0900

    authButtonFix

--- src/components/commons/LoginButton.tsx
+++ src/components/commons/LoginButton.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { useContext } from "react";
+import { auth } from "@/lib/firebase/client";
+import GoogleButton from "react-google-button";
+import "firebase/auth";
+import { Button } from "@mui/material";
+import { AuthContext } from "@/store/AuthContext";
 
 const LoginButton = () => {
-  return <div>LoginButton</div>;
-};
+  const { login } = useContext(AuthContext);
 
+  const handleLogin = async () => {
+    try {
+      await login();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return <Button onClick={handleLogin}>GoogleLogin</Button>;
+};
 export default LoginButton;
--- src/components/commons/LogoutButton.tsx
+++ src/components/commons/LogoutButton.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { Button } from "@mui/material";
+import { useContext } from "react";
+import { AuthContext } from "@/store/AuthContext";
+import { useRouter } from "next/router";
 
 const LogoutButton = () => {
-  return <div>LogoutButton</div>;
-};
+  const router = useRouter();
 
+  const { logout } = useContext(AuthContext);
+  const handleLogout = async () => {
+    try {
+      await logout();
+      router.push("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  return <Button onClick={handleLogout}>logout</Button>;
+};
 export default LogoutButton;
