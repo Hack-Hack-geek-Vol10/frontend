efe383cb282b06b454124cb019dd300d28854ddc
taku10101 <taku1011ht@gmail.com>
2023-11-28 16:55:11 +0900

    apolloProviderAdd

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { AuthProvider } from "@/store/AuthContext";
+import { ApolloProvider } from "@apollo/client";
+import client from "@/lib/apollo/client";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <ApolloProvider client={client}>
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ApolloProvider>
   );
 }
