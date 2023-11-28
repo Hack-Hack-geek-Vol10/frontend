c24a2d020a764324833c52755efed6a0cbccd1c8
taku10101 <taku1011ht@gmail.com>
2023-11-28 17:26:39 +0900

    tokenSet

--- src/lib/apollo/client.tsx
+++ src/lib/apollo/client.tsx
@@ -1,7 +1,23 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { useContext } from "react";
+import { AuthContext } from "@/store/AuthContext";
+const httpLink = createHttpLink({
+  uri: "http://localhost:8080/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = useContext(AuthContext).currentUser?.getIdToken();
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
