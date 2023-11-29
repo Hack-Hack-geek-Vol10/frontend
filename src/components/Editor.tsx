94e075e429c4deeafe3ee3a65fde69d8036ddd96
taku10101 <taku1011ht@gmail.com>
2023-11-30 02:22:45 +0900

    w

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,16 +1,15 @@
 import React from "react";
 import ReactAce from "react-ace/lib/ace";
-import { text } from "stream/consumers";
 import useEditor from "@/hooks/useEditor";
 const Editor = () => {
-  const { text, setText, width } = useEditor();
+  const { text, setText } = useEditor();
   return (
     <ReactAce
       theme='monokai'
       onChange={setText}
       value={text}
       name='UNIQUE_ID_OF_DIV'
-      width={width}
+      width='60%'
       editorProps={{ $blockScrolling: true }}
       height=' calc(100vh - 40px)'
     />
