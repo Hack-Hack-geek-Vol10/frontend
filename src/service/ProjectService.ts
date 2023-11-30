a223f356601976df4bcc2a0ed80c2b0d67febc18
taku10101 <taku1011ht@gmail.com>
2023-11-30 19:22:30 +0900

    callback del

--- src/service/ProjectService.ts
+++ src/service/ProjectService.ts
@@ -1,61 +1,61 @@
 import ProjectRepository from "@/repository/ProjectRepositories";
-import React from "react";
-export function useProjectService() {
-  const {
-    GetProjectRepository,
-    CreateProjectRepository,
-    UpdateProjectRepository,
-    DeleteProjectRepository,
-  } = ProjectRepository();
 
-  const GetProjectService = React.useCallback((projectId: string) => {
-    try {
-      const { data, loading, error } = GetProjectRepository(projectId);
-      if (loading) {
-        console.log("Loading");
-      }
-      if (error) {
-        console.log("Error");
-      }
-      return data;
-    } catch (error) {
-      console.log("ServicesError" + error);
+const {
+  GetProjectRepository,
+  CreateProjectRepository,
+  UpdateProjectRepository,
+  DeleteProjectRepository,
+} = ProjectRepository();
+
+const GetProjectService = async (projectId: string) => {
+  try {
+    const { data, loading, error } = GetProjectRepository(projectId);
+    if (loading) {
+      console.log("Loading");
     }
-  }, []);
-
-  const CreateProjectService = React.useCallback((title: string) => {
-    try {
-      const response = CreateProjectRepository(title);
-      return response;
-    } catch (error) {
-      console.log("ServicesError" + error);
+    if (error) {
+      console.log("Error");
     }
-  }, []);
+    return data;
+  } catch (error) {
+    console.log("ServicesError" + error);
+  }
+};
 
-  const UpdateProjectService = React.useCallback(
-    (projectId: string, title: string, lastImage: string) => {
-      try {
-        const response = UpdateProjectRepository(projectId, title, lastImage);
-        return response;
-      } catch (error) {
-        console.log("ServicesError" + error);
-      }
-    },
-    []
-  );
-  const DeleteProjectService = React.useCallback(async (projectId: string) => {
-    try {
-      const response = await DeleteProjectRepository(projectId);
-      return response;
-    } catch (error) {
-      console.log("ServicesError" + error);
-    }
-  }, []);
+const CreateProjectService = async (title: string) => {
+  try {
+    const response = CreateProjectRepository(title);
+    return response;
+  } catch (error) {
+    console.log("ServicesError" + error);
+  }
+};
 
-  return {
-    GetProjectService,
-    CreateProjectService,
-    UpdateProjectService,
-    DeleteProjectService,
-  };
-}
+const UpdateProjectService = async (
+  projectId: string,
+  title: string,
+  lastImage: string
+) => {
+  try {
+    const response = UpdateProjectRepository(projectId, title, lastImage);
+    return response;
+  } catch (error) {
+    console.log("ServicesError" + error);
+  }
+};
+
+const DeleteProjectService = async (projectId: string) => {
+  try {
+    const response = await DeleteProjectRepository(projectId);
+    return response;
+  } catch (error) {
+    console.log("ServicesError" + error);
+  }
+};
+
+export {
+  GetProjectService,
+  CreateProjectService,
+  UpdateProjectService,
+  DeleteProjectService,
+};
