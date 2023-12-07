3f7547a32158c0178cd854f810f39dc6a0a80aca
taku10101 <taku1011ht@gmail.com>
2023-12-07 16:19:07 +0900

    fix get user

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,19 @@
-import Image from "next/image";
-import { Box } from "@/lib/mui/muiRendering";
 import Header from "@/components/commons/CommonsHeader";
 import { AuthContext } from "@/store/AuthContext";
 import { useContext } from "react";
 import Typography from "@mui/material/Typography";
+import { useGetUser } from "@/service/useUserServices";
 export default function Home() {
   const { currentUser } = useContext(AuthContext);
+  const userId = currentUser?.uid;
+  const { data: userData } = useGetUser(userId!);
 
   return (
     <>
       <Header />
       <Typography variant='body1' color='initial'>
-        {currentUser?.displayName}
-      </Typography>{" "}
+        {userData?.user?.name}さん、ようこそ！
+      </Typography>
     </>
   );
 }
