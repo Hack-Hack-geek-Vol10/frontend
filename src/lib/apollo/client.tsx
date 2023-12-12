b0587ad4a484dcbff6701bf3b3314594a880e929
taku10101 <taku1011ht@gmail.com>
2023-12-12 22:12:57 +0900

    del upload

--- src/lib/apollo/client.tsx
+++ src/lib/apollo/client.tsx
@@ -1,7 +1,6 @@
-import { createUploadLink } from "apollo-upload-client";
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
-const uploadLink = createUploadLink({
+const httpLink = createHttpLink({
   uri: process.env.NEXT_PUBLIC_DEVELOP_BASE_URL,
 });
 
@@ -16,7 +15,7 @@ const authLink = setContext((_, { headers }) => {
 });
 
 const client = new ApolloClient({
-  link: authLink.concat(uploadLink),
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
